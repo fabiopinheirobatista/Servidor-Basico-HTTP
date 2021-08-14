a3ba718c2f4aae9b7619a91572c683f3d5d42a8e
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-08-14 11:58:36 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+    res.send('Olá mundo!!!')
+    console.log('Server running on http://localhost:3000')
+})
+
+app.listen(3000)
